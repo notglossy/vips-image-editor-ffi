755c279a57d909593be2f7197fff21885592d0cf
notglossy <3867591+notglossy@users.noreply.github.com>
2026-03-19 17:36:16 +0800

    Limit test() to supported mime types
    Replace the previous parent::test($args) delegation with an explicit mime type check in test(). If args['mime_type'] is set and not supported, return false; otherwise return true. This ensures the VIPS FFI editor only reports availability for MIME types it actually supports and avoids relying on parent class behavior.

--- classes/class-image-editor-vips-ffi.php
+++ classes/class-image-editor-vips-ffi.php
@@ -69,7 +69,10 @@ class Image_Editor_Vips_FFI extends \WP_Image_Editor {
 		} catch ( \Exception $e ) {
 			return false;
 		}
-		return parent::test( $args );
+		if ( isset( $args['mime_type'] ) && ! static::supports_mime_type( $args['mime_type'] ) ) {
+			return false;
+		}
+		return true;
 	}
 
 	/**
